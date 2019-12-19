26e0aa0595b07a3bdfaab37ffd91048ef5c62cea
keisuke713 <nebakei.tkb713@gmail.com>
2019-12-19 17:50:26 +0900

    make it possible to display error message when user input string

--- lib/calculator/runner.rb
+++ lib/calculator/runner.rb
@@ -6,11 +6,11 @@ module Calculator
         num1 = gets.chomp      
         print '二つ目の数字を入力してください:'
         num2 = gets.chomp
-        if (num1.class != Integer) || (num2.class != Integer)
+        if (num1 =~ /\D/) || (num2 =~ /\D/)
           puts '数字以外の物が入力されています。やり直してください'
           next
         end
-        sum_num = num1 + num2
+        sum_num = num1.to_i + num2.to_i
         puts "#{num1}と#{num2}を足した数字は#{sum_num}です"
         break
       end
