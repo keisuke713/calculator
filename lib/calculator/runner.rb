3594c43ff8ebd06f900d6cfa5d183130c461b81b
keisuke713 <nebakei.tkb713@gmail.com>
2019-12-21 10:42:38 +0900

    add test when user input string

--- lib/calculator/runner.rb
+++ lib/calculator/runner.rb
@@ -1,17 +1,20 @@
 module Calculator
   class Runner
     def self.run
+      new.run
+    end
+    def run
       loop do
         print '一つ目の数字を入力してください:'
-        num1 = gets.chomp      
+        num1 = STDIN.gets.chomp      
         print '二つ目の数字を入力してください:'
-        num2 = gets.chomp
+        num2 = STDIN.gets.chomp
         if (num1 =~ /\D/) || (num2 =~ /\D/)
-          puts '数字以外の物が入力されています。やり直してください'
+          return '数字以外の物が入力されています。やり直してください'
           next
         end
         sum_num = num1.to_i + num2.to_i
-        puts "#{num1}と#{num2}を足した数字は#{sum_num}です"
+        return "#{num1}と#{num2}を足した数字は#{sum_num}です"
         break
       end
     end
--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -1,20 +1,32 @@
 RSpec.describe Calculator::Runner do
   shared_examples 'return 数字以外の物が入力されています' do
-    it { expect(1 + 2).to eq 3 }
+    it { expect(Calculator::Runner.run).to eq '数字以外の物が入力されています。やり直してください' }
   end
 
   context 'num1 is wrong' do
+    before do
+      allow(STDIN).to receive(:gets).and_return('a', '1')
+    end
     it_behaves_like 'return 数字以外の物が入力されています'
   end
   context 'num2 is wrong' do
+    before do
+      allow(STDIN).to receive(:gets).and_return('1', 'a')
+    end
     it_behaves_like 'return 数字以外の物が入力されています'
   end
   context 'num1 and num2 are wrong' do
+    before do
+      allow(STDIN).to receive(:gets).and_return('a', 'a')
+    end
     it_behaves_like 'return 数字以外の物が入力されています'
   end
   context 'num1 and num2 is correct' do
+    before do
+      allow(STDIN).to receive(:gets).and_return('2', '1')
+    end
     it 'return add nums' do
-      expect(1 + 2).to eq 3
+      expect(Calculator::Runner.run).to eq '2と1を足した数字は3です'
     end
   end
 end
